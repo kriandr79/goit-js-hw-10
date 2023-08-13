5dcc0b051a81bd02798f5499251ca9f4762930ac
kriandr79 <127450901+kriandr79@users.noreply.github.com>
2023-08-13 11:25:10 +0300

    task 1

--- src/index.js
+++ src/index.js
@@ -1,33 +1,50 @@
+import Notiflix from 'notiflix';
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
 const refs = {
   selectEl: document.querySelector('.breed-select'),
-  loaderEl: document.querySelector('loader'),
-  errorEl: document.querySelector('error'),
-  catInfoEl: document.querySelector('cat-info'),
+  loaderEl: document.querySelector('.loader'),
+  errorEl: document.querySelector('.error'),
+  catInfoEl: document.querySelector('.cat-info'),
 };
-
-// console.log(refs.selectEl);
 
 refs.selectEl.addEventListener('change', onSelectChange);
 
+// Невідображається елементи селекту та помилки
+toggleClass(refs.selectEl);
+toggleClass(refs.errorEl);
+
+// Завантаження списку порід котів
 fetchBreeds()
   .then(data => {
     refs.selectEl.innerHTML = createCatListMarkup(data);
+    toggleClass(refs.selectEl);
+    toggleClass(refs.loaderEl);
   })
   .catch(error => {
-    console.error(error);
+    toggleClass(refs.loaderEl);
+    toggleClass(refs.errorEl);
   });
 
-
+// Функція вибору кота 
 function onSelectChange(e) {
-  //   console.log(e.currentTarget.value);
+  // для проби
+  Notiflix.Notify.info('Loading data, please wait...', {
+    timeout: 1000,
+  });
+
+  toggleClass(refs.loaderEl);
+  toggleClass(refs.catInfoEl);
+
   fetchCatByBreed(e.currentTarget.value)
     .then(data => {
-      console.log(data);
+      refs.catInfoEl.innerHTML = createCatSampleMarkup(data[0]);
+      toggleClass(refs.catInfoEl);
+      toggleClass(refs.loaderEl);
     })
     .catch(error => {
-      console.error(error);
+      toggleClass(refs.loaderEl);
+      toggleClass(refs.errorEl);
     });
 }
 
@@ -38,7 +55,10 @@ function createCatListMarkup(arr) {
 }
 
 function createCatSampleMarkup(arr) {
-    return arr
-      .map(({ id, name }) => `<option value="${id}">${name}</option>`)
-      .join('');
+  const { breeds, url } = arr;
+  return `<img src="${url}" width="400" alt="${breeds[0].name}"/><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b><p>${breeds[0].temperament}</p>`;
+}
+
+function toggleClass(element) {
+  element.classList.toggle('is-hidden');
 }
